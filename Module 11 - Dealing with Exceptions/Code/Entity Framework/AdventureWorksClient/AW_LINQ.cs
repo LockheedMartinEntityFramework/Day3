e2f6e163961cad159ba80ad34ba7c681d1d31102
Jeff McBride <jeff@geeklearn.com>
2015-09-23 13:13:35 -0400

    Added Entity SQL

--- Module 11 - Dealing with Exceptions/Code/Entity Framework/AdventureWorksClient/AW_LINQ.cs
+++ Module 11 - Dealing with Exceptions/Code/Entity Framework/AdventureWorksClient/AW_LINQ.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.Entity;
 using System.Diagnostics;
+using System.Data.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Data.EntityClient;
 
 namespace AdventureWorksClient
 {
@@ -27,142 +30,142 @@ namespace AdventureWorksClient
                 stopWatch.Start();
 
                 //// select * from SalesOrderHeaders
-                //var Sales =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    select sales;
 
-                //var Sales = awEntities.SalesOrderHeaders;
+                //var sales = awEntities.SalesOrderHeaders;
 
 
                 //// select AccountNumber from SalesOrderHeaders
-                //var Sales =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    select sales.AccountNumber;
 
-                //var Sales = awEntities.SalesOrderHeaders.Select(h => new { h.AccountNumber });  
+                //var sales = awEntities.SalesOrderHeaders.Select(h => new { h.AccountNumber });  
 
 
                 //// select AccountNumber and CustomerID from SalesOrderHeaders
-                //var Sales =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    select new { sales.AccountNumber, sales.CustomerID };
 
-                //var Sales = awEntities.SalesOrderHeaders.Select(h => new { h.AccountNumber, h.CustomerID });  
+                //var sales = awEntities.SalesOrderHeaders.Select(h => new { h.AccountNumber, h.CustomerID });  
 
 
                 //// select AccountNumber as Account and CustomerID as Customer from SalesOrderHeaders
-                //var SalesList =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    select new { Account = sales.AccountNumber, Customer = sales.CustomerID };
 
-                //var Sales = awEntities.SalesOrderHeaders.Select(h => new { Account = h.AccountNumber, Customer = h.CustomerID });  
+                //var sales = awEntities.SalesOrderHeaders.Select(h => new { Account = h.AccountNumber, Customer = h.CustomerID });  
 
 
                 //// select * from SalesOrderHeaders where TotalDue > 5000
-                //var Sales =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    where sales.TotalDue > 5000
                 //    select sales;
 
-                //var Sales = awEntities.SalesOrderHeaders.Where(h => h.TotalDue > 5000);  
+                //var sales = awEntities.SalesOrderHeaders.Where(h => h.TotalDue > 5000);  
 
 
                 //// select * from SalesOrderHeaders where TotalDue > 5000 and TotalDue < 10000
-                //var Sales =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    where sales.TotalDue > 5000 && sales.TotalDue < 10000
                 //    select sales;
 
-                //var Sales = awEntities.SalesOrderHeaders.Where(h => h.TotalDue > 5000 && h.TotalDue < 10000);  
+                //var sales = awEntities.SalesOrderHeaders.Where(h => h.TotalDue > 5000 && h.TotalDue < 10000);  
 
 
                 //// select * from SalesOrderHeaders where TotalDue < 5000 or TotalDue > 10000
-                //var Sales =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    where sales.TotalDue < 5000 || sales.TotalDue > 10000
                 //    select sales;
 
-                //var Sales = awEntities.SalesOrderHeaders.Where(h => h.TotalDue < 5000 || h.TotalDue > 10000);  
+                //var sales = awEntities.SalesOrderHeaders.Where(h => h.TotalDue < 5000 || h.TotalDue > 10000);  
 
 
                 //// select * from SalesOrderHeaders where OrderDate >= '12/1/2007'
-                //var Sales =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    where sales.OrderDate >= new DateTime(2007, 12, 1)
                 //    select sales;
 
-                //var Sales = awEntities.SalesOrderHeaders.Where(h => h.OrderDate >= new DateTime(2007, 12, 1));  
+                //var sales = awEntities.SalesOrderHeaders.Where(h => h.OrderDate >= new DateTime(2007, 12, 1));  
 
 
                 //// select * from SalesOrderHeaders where SalesOrderID in (43659, 43660, 43661)
                 //int[] salesOrderIDs = { 43659, 43660, 43661 };
-                //var Sales =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    where salesOrderIDs.Contains(sales.SalesOrderID)
                 //    select sales;
 
                 //int[] salesOrderIDs = { 43659, 43660, 43661 };
-                //var Sales = awEntities.SalesOrderHeaders.Where(h => salesOrderIDs.Contains(h.SalesOrderID));  
+                //var sales = awEntities.SalesOrderHeaders.Where(h => salesOrderIDs.Contains(h.SalesOrderID));  
 
 
                 //// select * from SalesOrderHeaders order by OrderDate
-                //var Sales =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    orderby sales.OrderDate
                 //    select sales;
 
-                //var Sales = awEntities.SalesOrderHeaders.OrderBy(h => h.OrderDate);  
+                //var sales = awEntities.SalesOrderHeaders.OrderBy(h => h.OrderDate);  
 
 
                 //// select * from SalesOrderHeaders order by OrderDate desc
-                //var Sales =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    orderby sales.OrderDate descending
                 //    select sales;
 
-                //var Sales = awEntities.SalesOrderHeaders.OrderByDescending(h => h.OrderDate);  
+                //var sales = awEntities.SalesOrderHeaders.OrderByDescending(h => h.OrderDate);  
 
 
                 //// select * from SalesOrderHeaders order by OrderDate, ShipDate
-                //var Sales =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    orderby sales.OrderDate, sales.ShipDate
                 //    select sales;
 
-                //var Sales = awEntities.SalesOrderHeaders.OrderBy(h => h.OrderDate).ThenBy(h => h.ShipDate);  
+                //var sales = awEntities.SalesOrderHeaders.OrderBy(h => h.OrderDate).ThenBy(h => h.ShipDate);  
 
 
                 //// select ShipMethodID, Count(*) from SalesOrderHeaders group by ShipMethodID
-                //var Sales =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    group sales by sales.ShipMethodID into salesGroup
                 //    select new { ShipMethodID = salesGroup.Key, Count = salesGroup.Count() };
 
-                //var Sales = awEntities.SalesOrderHeaders.GroupBy(h => h.ShipMethodID).Select(g => new { ShipMethodID = g.Key, Count = g.Count() });
+                //var sales = awEntities.SalesOrderHeaders.GroupBy(h => h.ShipMethodID).Select(g => new { ShipMethodID = g.Key, Count = g.Count() });
 
 
                 //// select ShipMethodID, Count(*) from SalesOrderHeaders where TotalDue > 1000 group by ShipMethodID
-                //var Sales =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    where sales.TotalDue > 1000
                 //    group sales by sales.ShipMethodID into salesGroup
                 //    select new { ShipMethodID = salesGroup.Key, Count = salesGroup.Count() };
 
-                //var Sales = awEntities.SalesOrderHeaders
+                //var sales = awEntities.SalesOrderHeaders
                 //    .Where(h => h.TotalDue > 1000)
                 //    .GroupBy(h => h.ShipMethodID)
                 //    .Select(g => new { ShipMethodID = g.Key, Count = g.Count() });
 
 
                 //// select ShipMethodID, Count(*) from SalesOrderHeaders where TotalDue > 1000 group by ShipMethodID having Count(*) > 1000
-                //var Sales =
+                //var sales =
                 //    from sales in awEntities.SalesOrderHeaders
                 //    where sales.TotalDue > 1000
                 //    group sales by sales.ShipMethodID into salesGroup
                 //    where salesGroup.Count() > 1000
                 //    select new { ShipMethodID = salesGroup.Key, Count = salesGroup.Count() };
 
-                //var Sales = awEntities.SalesOrderHeaders
+                //var sales = awEntities.SalesOrderHeaders
                 //    .Where(h => h.TotalDue > 1000)
                 //    .GroupBy(h => h.ShipMethodID)
                 //    .Where(g => g.Count() > 1000)
@@ -171,13 +174,13 @@ namespace AdventureWorksClient
 
                 //// select headers.SalesOrderID, details.UnitPrice 
                 //// from SalesOrderHeaders as headers join SalesOrderDetails as details on headers.SalesOrderID = details.SalesOrderID
-                //var Sales =
+                //var sales =
                 //    from headers in awEntities.SalesOrderHeaders
                 //    join details in awEntities.SalesOrderDetails
                 //    on headers.SalesOrderID equals details.SalesOrderID
                 //    select new { headers.SalesOrderID, details.UnitPrice };
 
-                //var Sales = awEntities.SalesOrderHeaders
+                //var sales = awEntities.SalesOrderHeaders
                 //    .Join(
                 //        awEntities.SalesOrderDetails, 
                 //        d => d.SalesOrderID, 
@@ -186,29 +189,46 @@ namespace AdventureWorksClient
                 //    );
                 
                 //// IQueryable vs IEnumerable Performance
-                var Sales = awEntities.SalesOrderDetails
-                    .Where(s => s.UnitPrice > 1)
-                    .OrderBy(s => s.UnitPrice)
-                    .Take(1);
-
-                //IQueryable<SalesOrderDetail> Sales = awEntities.SalesOrderDetails
+                //var sales = awEntities.SalesOrderDetails
                 //    .Where(s => s.UnitPrice > 1)
                 //    .OrderBy(s => s.UnitPrice)
                 //    .Take(1);
 
-                //IEnumerable<SalesOrderDetail> Sales = awEntities.SalesOrderDetails
+                //IQueryable<SalesOrderDetail> sales = awEntities.SalesOrderDetails
                 //    .Where(s => s.UnitPrice > 1)
                 //    .OrderBy(s => s.UnitPrice)
                 //    .Take(1);
 
-                var results = Sales.ToList();
-                
-                stopWatch.Stop();
-                TimeSpan ts = stopWatch.Elapsed;
-                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
-                MessageBox.Show(String.Format("Elapsed Time = {0} and Count = {1}", elapsedTime, Sales.Count()));
+                //IEnumerable<SalesOrderDetail> sales = awEntities.SalesOrderDetails
+                //    .Where(s => s.UnitPrice > 1)
+                //    .OrderBy(s => s.UnitPrice)
+                //    .Take(1);
 
-                this.dataGridViewQueryResults.DataSource = results;
+                //List<SalesOrderHeader> results = sales.ToList();
+                //stopWatch.Stop();
+                //TimeSpan ts = stopWatch.Elapsed;
+                //string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+                //MessageBox.Show(String.Format("Elapsed Time = {0} and Count = {1}", elapsedTime, results.Count()));
+
+                //this.dataGridViewQueryResults.DataSource = sales;
+
+
+                //// Entity SQL
+                //String entitySQL = "SELECT VALUE soh FROM SalesOrderHeaders as soh";
+                //String entitySQL = "SELECT VALUE soh FROM SalesOrderHeaders as soh WHERE soh.TotalDue > 5000";
+                //String entitySQL = "SELECT VALUE soh FROM SalesOrderHeaders as soh WHERE soh.TotalDue > 5000 and soh.TotalDue < 10000";
+                //String entitySQL = "SELECT VALUE soh FROM SalesOrderHeaders as soh WHERE soh.SalesOrderID IN MultiSet (43659, 43660, 43661)";
+                //String entitySQL = "SELECT VALUE soh FROM SalesOrderHeaders as soh order by soh.OrderDate";
+                //String entitySQL = "SELECT VALUE soh FROM SalesOrderHeaders as soh order by soh.OrderDate desc";
+                //String entitySQL = "SELECT VALUE soh FROM SalesOrderHeaders as soh order by soh.OrderDate, soh.ShipDate desc";
+                //String entitySQL = "SELECT VALUE soh FROM SalesOrderHeaders as soh JOIN SalesOrderDetails as sod ON soh.SalesOrderID = sod.SalesOrderID";
+
+
+                //// Create a query object.
+                //var sales = (awEntities as IObjectContextAdapter).ObjectContext.CreateQuery<SalesOrderHeader>(entitySQL);
+
+                //// Display Results
+                //    this.dataGridViewQueryResults.DataSource = sales;
             }
         }
     }
