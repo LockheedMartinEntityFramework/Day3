466d23bf7721a697caaa8196f86109ccc5962cb0
Jeff McBride <jeff@geeklearn.com>
2015-09-23 07:14:42 -0400

    Day3 Files

---
+++ Module 6 - Manipulating Data using LINQ/Code/Entity Framework/EDMX Database First/CustomerComputedColumns.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDMX_Database_First
+{
+    public partial class Customer
+    {
+        public string FullName { get { return String.Format("{0} {1}", FirstName, LastName); } }
+    }
+}
---
+++ Module 7 - Manipulating Data using Entity SQL/Code/Entity Framework/TestClient/EFMasterDetails.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using EFWorkflow = EDMX_Database_First;
+using System.Data.Entity;
+
+namespace TestClient
+{
+    public partial class EFMasterDetails : Form
+    {
+        private EFWorkflow.CustomerContextContainer customerContext = new EFWorkflow.CustomerContextContainer();
+
+        public EFMasterDetails()
+        {
+            InitializeComponent();
+
+            customerContext.Customers.Load();
+            //customerContext.Purchases.Load();
+            //customerContext.Customers.Include(c => c.Purchases).Load();
+        }
+
+        private void EFMasterDetails_Load(object sender, EventArgs e)
+        {
+            this.customerBindingSource.DataSource = this.customerContext.Customers.Local.ToBindingList();
+        }
+
+        private void customerBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customerContext.SaveChanges();
+        }
+    }
+}
